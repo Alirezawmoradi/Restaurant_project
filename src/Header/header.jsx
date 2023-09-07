b492e78e523b8ca2c1d012f641d3fa207ed2b0cc
KMflutter <kmflutterteam@gmail.com>
2023-09-08 00:01:09 +0330

    first settings of app

---
+++ src/Header/header.jsx
@@ -0,0 +1,12 @@
+const Header=()=>{
+    return (
+        <header className='bg-size-cover bg-position-center'>
+            <div className='container'>
+                <h1>
+                    غذای رژیمی
+                </h1>
+            </div>
+        </header>
+    )
+}
+export default Header;
