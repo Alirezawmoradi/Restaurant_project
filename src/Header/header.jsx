01484cbc79794a39b331832d742d0f90e51ec939
KMflutter <kmflutterteam@gmail.com>
2023-09-08 00:25:32 +0330

    first settings of app Header chnaged

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,8 +1,9 @@
+import  './header.css'
 const Header=()=>{
     return (
-        <header className='bg-size-cover bg-position-center'>
+        <header className='bg-size-cover bg-position-center py-5'>
             <div className='container'>
-                <h1>
+                <h1 className='text-start text-light'>
                     غذای رژیمی
                 </h1>
             </div>
